6517dca0bdd3a4cff500733f3814aec0184d3fcf
Jithin <jithinraju95@gmail.com>
2020-02-19 18:39:59 +0000

    front end final

--- HealthFE/Clientscript/view.js
+++ HealthFE/Clientscript/view.js
@@ -17,23 +17,53 @@ window.Health.Landing.land = $.extend(true,
 
 
         var createViewModel = function() {
-
+vm.Activityitem=ko.observable();
+vm.userprofile=ko.observable();
         vm.Searchview=new Searchviewmodel();
-    vm.submitform= function()
+    vm.searchactivities= function()
         {
+          var location={
+            "latitude":53.88,
+           "longitude":-8.57
+          }
         $.ajax({
-            url: 'http://localhost:8080/authenticate',
+            url: 'http://health.com:5000/api/area_search',
             type: 'POST',
+            headers:{
+              "Access-Control-Allow-Origin":"*"
+            },
             contentType: 'application/json; charset=utf-8',
-            data:JSON.stringify(Searchview),
+            data:JSON.stringify(location),
             success: function(result){
-
+vm.Activityitem(result)
                    },
                    error: function(XMLHttpRequest, textStatus, errorThrown) {
                        alert(XMLHttpRequest.responseJSON.message);
                    }
         });
         }
+        vm.Loginuser= function()
+            {
+              var user={
+              "email":"abcdd",
+              "password":"abcd"
+              }
+            $.ajax({
+                url: 'http://health.com:5000/api/user/signin',
+                type: 'GET',
+                headers:{
+                  "Access-Control-Allow-Origin":"*"
+                },
+                contentType: 'application/json; charset=utf-8',
+                data:user,
+                success: function(result){
+    vm.userprofile(result);
+                       },
+                       error: function(XMLHttpRequest, textStatus, errorThrown) {
+                           alert(XMLHttpRequest.responseJSON.message);
+                       }
+            });
+            }
 
         }
            var init = function(opt) {
